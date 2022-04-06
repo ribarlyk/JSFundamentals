f23a851c25053323a74656330528353b311a9af8
PavelHristov <98495752+ribarlyk@users.noreply.github.com>
2022-04-06 14:01:43 +0300

    Delete 1.Fruit.js

--- 1.Fruit.js
+++
@@ -1,5 +0,0 @@
-function solve(string,buy,price){
-    let money = buy * price/1000;
-console.log(`I need \$${money.toFixed(2)} to buy ${(buy/1000).toFixed(2)} kilograms ${string}.`)
-}
-solve('apple', 1563, 2.35)
